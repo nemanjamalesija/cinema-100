c40121d3450e65d2d3e6214fe00196bb54d73b56
Nemanja Malesija <malesija13@gmail.com>
2023-03-08 04:53:00 +0100

    Setup router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
-import { useState } from 'react';
-import axios from 'axios';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import BookmakeredMovies from './pages/BookmakeredMovies';
+import SingleMovie from './pages/SingleMovie';
+import Error from './pages/Error';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path=":id" element={<SingleMovie />} />
+          <Route path="bookmarks" element={<BookmakeredMovies />} />
+          <Route path="bookmarks/:id" element={<SingleMovie />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
