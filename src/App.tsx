834e37876d162e669a3fad00bbbb3cf703fb95e0
Nemanja Malesija <malesija13@gmail.com>
2023-03-08 03:48:46 +0100

    Store the response from the API locally

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,25 @@
 import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const options = {
+    method: 'GET',
+    url: 'https://imdb-top-100-movies.p.rapidapi.com/',
+    headers: {
+      'X-RapidAPI-Key': 'ca15920c56mshf9c581b5644b811p131238jsnb4402ca00e5c',
+      'X-RapidAPI-Host': 'imdb-top-100-movies.p.rapidapi.com',
+    },
+  };
+
+  axios
+    .request(options)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+
   return <div className="App"></div>;
 }
 
