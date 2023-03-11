34ef094799af3e324906d55ca79fb52ed6beef2b
Nemanja Malesija <malesija13@gmail.com>
2023-03-11 08:17:54 +0100

    Style sidebar and pagination

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,7 +3,25 @@ import './sidebar.css';
 const Sidebar = () => {
   return (
     <aside className="sidebar">
-      <div className="sidebar__search"></div>
+      <div className="sidebar__search">
+        <div className="search-div">
+          <input type="text" className="sidebar__input" placeholder="Search" />
+          <button className="btn-search">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="icon__search"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10.5 3.75a6.75 6.75 0 100 13.5 6.75 6.75 0 000-13.5zM2.25 10.5a8.25 8.25 0 1114.59 5.28l4.69 4.69a.75.75 0 11-1.06 1.06l-4.69-4.69A8.25 8.25 0 012.25 10.5z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
       <div className="sidebar__personalize">
         <div className="sidebar__logo">
           <svg
