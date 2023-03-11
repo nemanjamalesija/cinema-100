416d1c0f6e3401be969ec9ab74bcc8c019257000
Nemanja Malesija <malesija13@gmail.com>
2023-03-11 03:03:37 +0100

    Refator home movies to separate pagination component

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import { useAppContext } from '../context';
+import MovieCard from './MovieCard';
+
+const Pagination = () => {
+  const {
+    state: { movies },
+  } = useAppContext();
+  const [moviesHomeIndex, setMoviesHomeIndex] = useState(0);
+  return (
+    <section className="section section__pagination">
+      <div className="container container__pagination">
+        <h2 className="heading--secondary heading-movies">
+          Recomended for you
+        </h2>
+        <div className="container__movies">
+          {movies[moviesHomeIndex].map((movie, i) => {
+            return <MovieCard key={movie.imdbid} {...movie} />;
+          })}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Pagination;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,29 +1,11 @@
-import { useState } from 'react';
-import MovieCard from '../components/MovieCard';
+import Pagination from '../components/Pagination';
 import Trending from '../components/Trending';
-import { useAppContext } from '../context';
 
 const Home = () => {
-  const {
-    state: { movies },
-  } = useAppContext();
-  const [moviesHomeIndex, setMoviesHomeIndex] = useState(0);
-
-  console.log(movies);
-
   return (
     <section className="section section__home">
       <Trending />
-      <div className="container container__home">
-        <h2 className="heading--secondary heading-movies">
-          Recomended for you
-        </h2>
-        <div className="container__movies">
-          {movies[moviesHomeIndex].map((movie, i) => {
-            return <MovieCard key={movie.imdbid} {...movie} />;
-          })}
-        </div>
-      </div>
+      <Pagination />
     </section>
   );
 };
