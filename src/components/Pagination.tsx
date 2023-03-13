d35136a105f457110b656b4e3f4fa87adea4dfcd
nemanjamalesija <malesija13@gmail.com>
2023-03-13 06:31:13 +0100

    Handle title change for pagination

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,13 +1,24 @@
+import { useAppContext } from '../context';
 import HomeMovies from './HomeMovies';
 import PagiationButtons from './PagiationButtons';
 import Sidebar from './Sidebar';
 
 const Pagination = () => {
+  const {
+    state: {
+      filters: { genre },
+    },
+  } = useAppContext();
+
   return (
-    <section className="section section__pagination">
+    <section className='section section__pagination'>
       <Sidebar />
-      <main className="container__pagination">
-        <h2 className="heading--secondary heading-movies">Top 100</h2>
+      <main className='container__pagination'>
+        <h2 className='heading--secondary heading-movies'>
+          {genre === 'All'
+            ? 'Top 100'
+            : genre.slice(0, 1).toUpperCase() + genre.slice(1)}
+        </h2>
         <HomeMovies />
         <PagiationButtons />
       </main>
