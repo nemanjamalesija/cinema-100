e022bf1b7629bc501de557b24687a3cb4650f0d7
nemanjamalesija <malesija13@gmail.com>
2023-03-17 18:33:02 +0100

    Style top part of the single movie

--- src/pages/SingleMovie.tsx
+++ src/pages/SingleMovie.tsx
@@ -1,5 +1,6 @@
 import { useParams } from 'react-router-dom';
 import { useAppContext } from '../context';
+import star from '../utils//icons/star.png';
 import './singleVideo.css';
 
 const SingleMovie = () => {
@@ -29,16 +30,47 @@ const SingleMovie = () => {
 
   return (
     <article className='single__video--container'>
-      <div className='single__video--photo'></div>
-      <div className='react__player--div'>
-        <iframe
-          className='embeded-video'
-          src={trailer}
-          frameBorder='0'
-          allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-          allowFullScreen
-          title='Embedded youtube'
-        />
+      <div className='single__video--textual-info  u--justify--space--between'>
+        <div className='single__video--textual--left u--align--center'>
+          <h2 className='heading--secondary heading--secondary-single-movie'>
+            {title}
+          </h2>
+          <span className='single__video--textual-year'>({year})</span>
+        </div>
+        <div className='single__video--textual--right u--justify--space--between'>
+          <div className='single__video--textial--right-info'>
+            <h3 className='heading--tertiary'>Imdb rating</h3>
+            <span>
+              <img
+                className='movie-card__star single__movie--star'
+                alt='rating'
+                src={star}
+              />
+            </span>
+            <span className='single__video--imdb-info'>{rating} </span>
+            <span className='single__video--imdb-subinfo'> / 10</span>
+          </div>
+          <div className='single__video--textial--right-info'>
+            <h3 className='heading--tertiary'>Rank</h3>
+            <span className='single__video--imdb-info'>{rank}</span>
+            <span className='single__video--imdb-subinfo'> / 100</span>
+          </div>
+        </div>
+      </div>
+      <div className='sinlge__video--trailer--image'>
+        <div className='single__video--photo--container'>
+          <img src={image} alt={title} className='single__video--image' />
+        </div>
+        <div className='react__player--div'>
+          <iframe
+            className='embeded-video'
+            src={trailer}
+            frameBorder='0'
+            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+            allowFullScreen
+            title='Embedded youtube'
+          />
+        </div>
       </div>
     </article>
   );
