37dc8422f6011409c554f822ac76ce86756725ed
nemanjamalesija <malesija13@gmail.com>
2023-03-17 17:29:36 +0100

    Embed trailer from youtube

--- src/pages/SingleMovie.tsx
+++ src/pages/SingleMovie.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useAppContext } from '../context';
+import './singleVideo.css';
 
 const SingleMovie = () => {
   const { id } = useParams();
@@ -11,7 +11,37 @@ const SingleMovie = () => {
   const singleMovie = movies.flat().find((mov) => mov.imdbid === id);
   console.log(singleMovie);
 
-  return <div>SingleMovie</div>;
+  if (!singleMovie) return <h2 className='heading--secondary'>Loading...</h2>;
+
+  const {
+    description,
+    director,
+    genre,
+    image,
+    imdbid,
+    rank,
+    rating,
+    title,
+    trailer,
+    writers,
+    year,
+  } = singleMovie;
+
+  return (
+    <article className='single__video--container'>
+      <div className='single__video--photo'></div>
+      <div className='react__player--div'>
+        <iframe
+          className='embeded-video'
+          src={trailer}
+          frameBorder='0'
+          allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+          allowFullScreen
+          title='Embedded youtube'
+        />
+      </div>
+    </article>
+  );
 };
 
 export default SingleMovie;
