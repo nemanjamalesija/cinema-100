dcabddb732acd8d61f8bbedc0da592b1faf2909f
nemanjamalesija <malesija13@gmail.com>
2023-03-19 19:25:09 +0100

    Start building form

--- src/pages/LogInPage.tsx
+++ src/pages/LogInPage.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import logo from './logo.png';
 
 const LogInPage = () => {
-  return <div>LogInPage</div>;
+  return (
+    <div className='container__form'>
+      <form className='log--in--form'></form>
+    </div>
+  );
 };
 
 export default LogInPage;
