e917c29eae7cad6c70f5dcffaa79190a3844a4d3
nemanjamalesija <malesija13@gmail.com>
2023-03-20 17:22:42 +0100

    Set states for the form, start sign in function

--- src/pages/LogInPage.tsx
+++ src/pages/LogInPage.tsx
@@ -1,15 +1,38 @@
 import logo from '../utils/images/logo.png';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+
+import { Auth } from 'firebase/auth';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
 
 const LogInPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const navigate = useNavigate();
+
+  const signIn = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    navigate('/home');
+  };
+
   return (
     <div className='container__form'>
       <form className='log--in--form'>
-        <input type='email' />
-        <input type='password' />
-        <Link className='link--nav reset' to='/home'>
-          <button type='submit'>Log in</button>
-        </Link>
+        <input
+          type='email'
+          value={email}
+          onChange={(e) => setEmail(e.currentTarget.value)}
+        />
+        <input
+          type='password'
+          value={password}
+          onChange={(e) => setPassword(e.currentTarget.value)}
+        />
+        <button type='submit' onClick={signIn}>
+          Log in
+        </button>
       </form>
     </div>
   );
