2d214d0399aa37d7a46d168f5e87f12ad643bd08
nemanjamalesija <malesija13@gmail.com>
2023-03-20 11:25:04 +0100

    install firebase tools

---
+++ src/config/firebase.ts
@@ -0,0 +1,13 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBNtWd7J3pMiFqqjpkeRGq8pr6QBrgPtH0',
+  authDomain: 'cinema-100.firebaseapp.com',
+  projectId: 'cinema-100',
+  storageBucket: 'cinema-100.appspot.com',
+  messagingSenderId: '12062736803',
+  appId: '1:12062736803:web:4dc99bcb17b142a8e71a16',
+};
+
+const app = initializeApp(firebaseConfig);
