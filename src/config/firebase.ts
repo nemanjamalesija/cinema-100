325692b8aba7305ebf53d4f6d339d4a8df40c5ef
nemanjamalesija <malesija13@gmail.com>
2023-03-20 17:11:03 +0100

    use get auth

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyBNtWd7J3pMiFqqjpkeRGq8pr6QBrgPtH0',
@@ -11,3 +12,4 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
