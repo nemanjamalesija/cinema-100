d7601a6d3e3231101eb6983249ee8f5b09c30ea8
nemanjamalesija <malesija13@gmail.com>
2023-03-23 23:14:30 +0100

    Add log out to nav

--- src/pages/Navigation.tsx
+++ src/pages/Navigation.tsx
@@ -19,10 +19,29 @@ const Navigation = () => {
           <p className='nav__logo-p'>cinema 100</p>
         </div>
       </Link>
-      <div className='greeting--message'>
-        <h3 className='welcome--back-heading'>
-          Welcome back, <span>{name}</span>
-        </h3>
+      <div className='nav__right--side'>
+        <div className='greeting--message'>
+          <h3 className='welcome--back-heading'>
+            Welcome back, <span>{name}</span>
+          </h3>
+        </div>
+        <div className='nav__logout'>
+          <h3 className='welcome--back-heading'>Log out</h3>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth={1.5}
+            stroke='currentColor'
+            className='nav--icon'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M5.636 5.636a9 9 0 1012.728 0M12 3v9'
+            />
+          </svg>
+        </div>
       </div>
     </nav>
   );
