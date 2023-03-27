f4c46fa7c94841877f07ca899177cfdb8ad14169
nemanjamalesija <malesija13@gmail.com>
2023-03-27 23:45:59 +0200

    Refactor sign in form to separate component

---
+++ src/components/SignInForm.tsx
@@ -0,0 +1,137 @@
+import React, { useState } from 'react';
+import { initialize } from '../config/firebase';
+import { useNavigate } from 'react-router-dom';
+import { collection, getDocs, doc, setDoc } from 'firebase/firestore';
+import { signInWithEmailAndPassword, signInAnonymously } from 'firebase/auth';
+import { useAppContext } from '../context';
+
+type signInFormProps = {
+  redirectHandler: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  formToDispay: string;
+  logInError: string;
+  setLogInError: React.Dispatch<React.SetStateAction<string>>;
+};
+
+const SignInForm = ({
+  redirectHandler,
+  formToDispay,
+  logInError,
+  setLogInError,
+}: signInFormProps) => {
+  const { dispatch } = useAppContext();
+  const [emailLogIn, setEmailLogIn] = useState('');
+  const [passwordLogIn, setPasswordLogIn] = useState('');
+  const { db, auth } = initialize();
+  const navigate = useNavigate();
+
+  const signInHandler = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+
+    try {
+      await signInWithEmailAndPassword(auth, emailLogIn, passwordLogIn);
+
+      const usersCollection = collection(db, 'users');
+      const response = await getDocs(usersCollection);
+
+      const user = response.docs
+        .map((u) => ({
+          data: u.data(),
+          id: u.id,
+        }))
+        .map((u) => u.data)
+        .find((u) => u.email === emailLogIn);
+
+      if (user) {
+        dispatch({ type: 'SET_CURRENT_USER', payload: user });
+        navigate('/home');
+      } else throw new Error("You don't have an account");
+    } catch (error) {
+      if (error instanceof Error) {
+        if (error.message === 'Firebase: Error (auth/invalid-email).')
+          setLogInError('This email adress is not found');
+
+        if (error.message === 'Firebase: Error (auth/wrong-password).') {
+          setLogInError('Incorrect password');
+        }
+      } else console.error(error);
+    }
+  };
+
+  const signInAnonymouslyHandler = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+    try {
+      await signInAnonymously(auth);
+      const randomEmail = `anonymousGuest@${crypto.randomUUID()}.com`;
+      const currentUserRef = doc(db, `users/${randomEmail}`);
+
+      await setDoc(currentUserRef, {
+        name: 'Guest',
+        email: randomEmail,
+        bookmarkeredMovies: [],
+      });
+
+      dispatch({
+        type: 'SET_CURRENT_USER',
+        payload: { name: 'Guest', email: randomEmail, bookmarkeredMovies: [] },
+      });
+      navigate('/home');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <form
+      className={
+        formToDispay === 'login'
+          ? 'form-credentials'
+          : 'form-credentials hidden'
+      }
+    >
+      <h2 className='heading--secondary--form'>Log In</h2>
+      <div className='inputs--div'>
+        <div className='form--control'>
+          <input
+            type='email'
+            placeholder='Email'
+            value={emailLogIn}
+            onChange={(e) => setEmailLogIn(e.currentTarget.value)}
+          />
+        </div>
+        <div className='form--control'>
+          <input
+            type='password'
+            placeholder='Password'
+            value={passwordLogIn}
+            onChange={(e) => setPasswordLogIn(e.currentTarget.value)}
+          />
+        </div>
+      </div>
+      <button
+        type='submit'
+        className='btn--form-submit'
+        onClick={signInHandler}
+      >
+        Log in
+      </button>
+      <div className='login--redirect u--justify--center'>
+        <p className='login--redirect-p'> Don't have an account ?</p>
+        <button className='btn--redirect' onClick={redirectHandler}>
+          Sign up
+        </button>
+      </div>
+      <div className='login--redirect login--redirect-guest u--justify--center'>
+        <p className='login--redirect-p'> Or,</p>
+        <button className='btn--redirect' onClick={signInAnonymouslyHandler}>
+          Log in as guest
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default SignInForm;
--- src/pages/LogInPage.tsx
+++ src/pages/LogInPage.tsx
@@ -1,64 +1,26 @@
 import logo from '../utils/images/logo.png';
 import { useState } from 'react';
 import { initialize } from '../config/firebase';
-import { useNavigate } from 'react-router-dom';
+
 import { collection, getDocs, doc, setDoc } from 'firebase/firestore';
 import { useAppContext } from '../context';
 import {
   createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
   signInAnonymously,
 } from 'firebase/auth';
 import './login.css';
+import SignInForm from '../components/SignInForm';
 
 const LogInPage = () => {
-  const [emailLogIn, setEmailLogIn] = useState('');
-  const [passwordLogIn, setPasswordLogIn] = useState('');
+  const [logInError, setLogInError] = useState('');
   const [emailSignUp, setEmailSignUp] = useState('');
   const [passwordSignUp, setPasswordSignUp] = useState('');
   const [currentUSerName, setCurrentUSerName] = useState('');
   const [currentUserLastName, setCurrentUserLastName] = useState('');
-  const [logInError, setLogInError] = useState('');
   const [formToDispay, setFormToDisplay] = useState('login');
-  const navigate = useNavigate();
+
   const { db, auth } = initialize();
-
   const { dispatch } = useAppContext();
-
-  const signInHandler = async (
-    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  ) => {
-    e.preventDefault();
-
-    try {
-      await signInWithEmailAndPassword(auth, emailLogIn, passwordLogIn);
-
-      const usersCollection = collection(db, 'users');
-      const response = await getDocs(usersCollection);
-
-      const user = response.docs
-        .map((u) => ({
-          data: u.data(),
-          id: u.id,
-        }))
-        .map((u) => u.data)
-        .find((u) => u.email === emailLogIn);
-
-      if (user) {
-        dispatch({ type: 'SET_CURRENT_USER', payload: user });
-        navigate('/home');
-      } else throw new Error("You don't have an account");
-    } catch (error) {
-      if (error instanceof Error) {
-        if (error.message === 'Firebase: Error (auth/invalid-email).')
-          setLogInError('This email adress is not found');
-
-        if (error.message === 'Firebase: Error (auth/wrong-password).') {
-          setLogInError('Incorrect password');
-        }
-      } else console.error(error);
-    }
-  };
 
   const signUpHandler = async (
     e: React.MouseEvent<HTMLButtonElement, MouseEvent>
@@ -94,31 +56,6 @@ const LogInPage = () => {
     });
   };
 
-  const signInAnonymouslyHandler = async (
-    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  ) => {
-    e.preventDefault();
-    try {
-      await signInAnonymously(auth);
-      const randomEmail = `anonymousGuest@${crypto.randomUUID()}.com`;
-      const currentUserRef = doc(db, `users/${randomEmail}`);
-
-      await setDoc(currentUserRef, {
-        name: 'Guest',
-        email: randomEmail,
-        bookmarkeredMovies: [],
-      });
-
-      dispatch({
-        type: 'SET_CURRENT_USER',
-        payload: { name: 'Guest', email: randomEmail, bookmarkeredMovies: [] },
-      });
-      navigate('/home');
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   return (
     <section className='section__log-in'>
       <div className='container__forms u--justify--center u--align--center'>
@@ -127,56 +64,12 @@ const LogInPage = () => {
 
           <p className='login-logo-p'>cinema 100</p>
         </div>
-        <form
-          className={
-            formToDispay === 'login'
-              ? 'form-credentials'
-              : 'form-credentials hidden'
-          }
-        >
-          <h2 className='heading--secondary--form'>Log In</h2>
-          <div className='inputs--div'>
-            <div className='form--control'>
-              <input
-                type='email'
-                placeholder='Email'
-                value={emailLogIn}
-                onChange={(e) => setEmailLogIn(e.currentTarget.value)}
-              />
-            </div>
-            <div className='form--control'>
-              <input
-                type='password'
-                placeholder='Password'
-                value={passwordLogIn}
-                onChange={(e) => setPasswordLogIn(e.currentTarget.value)}
-              />
-            </div>
-          </div>
-          <button
-            type='submit'
-            className='btn--form-submit'
-            onClick={signInHandler}
-          >
-            Log in
-          </button>
-          <div className='login--redirect u--justify--center'>
-            <p className='login--redirect-p'> Don't have an account ?</p>
-            <button className='btn--redirect' onClick={redirectHandler}>
-              Sign up
-            </button>
-          </div>
-          <div className='login--redirect login--redirect-guest u--justify--center'>
-            <p className='login--redirect-p'> Or,</p>
-            <button
-              className='btn--redirect'
-              onClick={signInAnonymouslyHandler}
-            >
-              Log in as guest
-            </button>
-          </div>
-        </form>
-
+        <SignInForm
+          formToDispay={formToDispay}
+          redirectHandler={redirectHandler}
+          logInError={logInError}
+          setLogInError={setLogInError}
+        />
         <form
           className={
             formToDispay === 'signup'
